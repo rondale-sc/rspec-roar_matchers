d74c4c8a620193aeab6a06b3a0cc7116126df604
Jonathan Jackson <jonathan.jackson1@me.com>
2013-12-10 14:14:59 -0500

    Bump version.
    * keep compatible with roar implementation

--- lib/roar_matchers/support.rb
+++ lib/roar_matchers/support.rb
@@ -8,9 +8,9 @@ module RoarMatchers
   end
 
   def self.get_actual_property_names(representer)
-    representer.instance_variable_get("@representable_attrs").reject do |attr|
-      attr.options[:collection]
-    end.map(&:name)
+    representer.instance_variable_get("@representable_attrs").reject do |key, attrs|
+      attrs.options[:collection]
+    end.keys
   end
 
   def self.get_actual_link_names(representer, options={})
@@ -23,8 +23,8 @@ module RoarMatchers
   end
 
   def self.get_actual_collection_names(representer)
-    representer.instance_variable_get("@representable_attrs").select do |attr|
-      attr.options[:collection] && attr.name != "links_array"
-    end.map(&:name)
+    representer.instance_variable_get("@representable_attrs").select do |key, attrs|
+      attrs.options[:collection] && attrs.name != "links_array"
+    end.keys
   end
 end
--- rspec-roar_matchers.gemspec
+++ rspec-roar_matchers.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "roar"
+  spec.add_development_dependency "roar", "~> 0.12.3"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "pry"
 end
